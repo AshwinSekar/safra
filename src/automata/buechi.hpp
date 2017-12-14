8398e1f4f66fec19a744882364ca4e97320be6f4
Ashwin Sekar <asekar@andrew.cmu.edu>
2017-12-14 17:46:52 -0500

    Add the determinization outline

--- src/automata/buechi.hpp
+++ src/automata/buechi.hpp
@@ -2,14 +2,18 @@
  * The Buechi Automata type
  */
 
-#ifndef safra_BECHI_HPP_
-#define safra_BECHI_HPP_
+#ifndef safra_BUECHI_HPP_
+#define safra_BUECHI_HPP_
 
 #include "automata.hpp"
+#include "rabin.hpp"
 
 #include <set>
 
 namespace safra {
+
+  class SafraNode;
+  class SafraTree;
 
   class Buechi : public Automata {
     public:
@@ -21,13 +25,36 @@ namespace safra {
        */
       Buechi(const std::string input_file, bool _use_letters);
 
+
+      /**
+       * Convert to Rabin
+       */
+      Rabin to_rabin() const;
+
       /**
        * Implement superclass methods
        */
       virtual void create_representation(std::ostream& stream);
       friend std::ostream& operator<< (std::ostream& stream, const Buechi& buechi);
 
+    private:
+      // Helper methods to convert to rabin
+      SafraTree compute_transition(const SafraTree& tree, int letter);
+      // Step 1) Remove marks
+      SafraTree copy_and_remove_marks(const SafraTree& tree);
+      // Step 2) Branch accepting
+      void branch_accept(SafraTree& tree);
+      // Step 3) Powerset construction
+      void pset_const(SafraTree& tree, int letter);
+      // Step 4) Horizontal merge
+      void horizontal_merge(SafraTree& tree);
+      // Step 5) Remove empty
+      void remove_empty(SafraTree& tree);
+      // Step 6) Vertical merge
+      void vertical_merge(SafraTree& tree);
+
   };
+
 
 }
 
